a9c9d292dd75b20c8f1100673f67b8f997aca5a6
Vanitas007 <piotr.zajac07@gmail.com>
2022-09-20 22:55:50 +0200

    27-29 Casting, Primitive Types
    Casting
    Double, Float

---
+++ src/main/java/s3_FirstSteps/a29_FloatAndDouble.java
@@ -0,0 +1,24 @@
+package s3_FirstSteps;
+
+public class a29_FloatAndDouble {
+
+    public static void main(String[] args) {
+
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("float min Value " + myMinFloatValue);
+        System.out.println("float max value " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("double min Value " + myMinDoubleValue);
+        System.out.println("double max value " + myMaxDoubleValue);
+
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
+    }
+}
