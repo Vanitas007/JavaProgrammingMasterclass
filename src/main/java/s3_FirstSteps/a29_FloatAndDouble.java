8aca0ce2bbc0a190f706236881fa7a2a9f4e0f27
Vanitas007 <piotr.zajac07@gmail.com>
2022-09-21 22:51:24 +0200

    30-31 Primitive data types
    Float, double, char, boolean

--- src/main/java/s3_FirstSteps/a29_FloatAndDouble.java
+++ src/main/java/s3_FirstSteps/a29_FloatAndDouble.java
@@ -16,9 +16,28 @@ public class a29_FloatAndDouble {
         System.out.println("double max value " + myMaxDoubleValue);
 
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+//        int myIntValue = 5;
+//        float myFloatValue = 5.25f;
+//        double myDoubleValue = 5.25d;
 
+        //30.
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00d / 3.00d;
+        System.out.println("myIntValue= " + myIntValue);
+        System.out.println("myFloatValue= " + myFloatValue);
+        System.out.println("myDoubleValue= " + myDoubleValue);
+
+
+        double numberOfPounds = 5.5d;
+        double poundToKilogram = numberOfPounds * 0.45359237d;
+        System.out.println("Converted kilograms to pounds= " + poundToKilogram);
+
+
+        double pi = Math.PI;
+        double anotherNumber = 3000000.4567890d;
+        System.out.println(pi);
+        System.out.println(anotherNumber);
     }
 }
