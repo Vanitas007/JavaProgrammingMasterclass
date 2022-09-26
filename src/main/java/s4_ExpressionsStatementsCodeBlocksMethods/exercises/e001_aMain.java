708943d0304f01ae13724e7e8454e75e28095cff
Vanitas007 <piotr.zajac07@gmail.com>
2022-09-26 18:07:00 +0200

    CodingExercise_1_SpeedConverter
    Kilometers to miles/hour converter

---
+++ src/main/java/s4_ExpressionsStatementsCodeBlocksMethods/exercises/e001_aMain.java
@@ -0,0 +1,10 @@
+package s4_ExpressionsStatementsCodeBlocksMethods.exercises;
+
+public class e001_aMain {
+    public static void main(String[] args) {
+
+        long miles = e001_SpeedConverter.toMilesPerHour(10.5);
+        System.out.println(miles);
+        e001_SpeedConverter.printConversion(12);
+    }
+}
