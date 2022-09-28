e86ca76648c724c4455ef7bcc732f64043f318f4
Vanitas007 <piotr.zajac07@gmail.com>
2022-09-28 22:03:35 +0200

    CodingExercise_6_EqualSumChecker
    easy IF

---
+++ src/main/java/s4_ExpressionsStatementsCodeBlocksMethods/exercises/e006_EqualSumChecker.java
@@ -0,0 +1,9 @@
+package s4_ExpressionsStatementsCodeBlocksMethods.exercises;
+
+public class e006_EqualSumChecker {
+
+    public static boolean hasEqualSum(int numberOne, int numberTwo, int sum) {
+
+        return numberOne + numberTwo == sum;
+    }
+}
---
+++ src/main/java/s4_ExpressionsStatementsCodeBlocksMethods/exercises/e006_aMain.java
@@ -0,0 +1,8 @@
+package s4_ExpressionsStatementsCodeBlocksMethods.exercises;
+
+public class e006_aMain {
+    public static void main(String[] args) {
+
+        e006_EqualSumChecker.hasEqualSum(1,1,2);
+    }
+}
