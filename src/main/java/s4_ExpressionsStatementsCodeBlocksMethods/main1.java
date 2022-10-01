f1e6e0fee064640c1721e19d96e3a65675db6c02
Vanitas007 <piotr.zajac07@gmail.com>
2022-10-01 13:27:27 +0200

    61 Seconds And Minutes Bonus solution_Challenge
    Other way to present time, const

--- src/main/java/s4_ExpressionsStatementsCodeBlocksMethods/main1.java
+++ src/main/java/s4_ExpressionsStatementsCodeBlocksMethods/main1.java
@@ -2,7 +2,8 @@ package s4_ExpressionsStatementsCodeBlocksMethods;
 
 public class main1 {
     public static void main(String[] args) {
-        a060_SecondsAndMinutes_Challenge.getDurationString(6895,45);
-        a060_SecondsAndMinutes_Challenge.getDurationString(44444);
+        System.out.println(a060_SecondsAndMinutes_Challenge.getDurationString(6895, 9));
+        System.out.println(a060_SecondsAndMinutes_Challenge.getDurationString(44444));
+        System.out.println(a060_SecondsAndMinutes_Challenge.getDurationString(-42));
     }
 }
