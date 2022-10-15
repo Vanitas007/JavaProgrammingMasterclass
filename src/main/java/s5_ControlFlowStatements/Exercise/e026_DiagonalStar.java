33f9d6387bd1a244476caea7051aaa11e38b163c
Vanitas007 <piotr.zajac07@gmail.com>
2022-10-15 17:57:49 +0200

    CodingExercise_26_DiagonalStar

---
+++ src/main/java/s5_ControlFlowStatements/Exercise/e026_DiagonalStar.java
@@ -0,0 +1,26 @@
+package s5_ControlFlowStatements.Exercise;
+
+public class e026_DiagonalStar {
+    public static void main(String[] args) {
+        printSquareStar(2);
+    }
+
+    public static void printSquareStar(int number) {
+
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        } else {
+            for (int row = 1; row <= number; row++) {
+                for (int column = 1; column <= number; column++) {
+                    if (row == 1 || row == number || column == 1 || column == number || column == row ||
+                            column == number - row + 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println("");
+            }
+        }
+    }
+}
