be24619f242a46e7b3f99b8b88d30ba61cf1725f
Vanitas007 <piotr.zajac07@gmail.com>
2022-10-18 22:33:41 +0200

    CodingExercise_28_PaintJob_cz1

---
+++ src/main/java/s5_ControlFlowStatements/Exercise/e028_PaintJob.java
@@ -0,0 +1,21 @@
+package s5_ControlFlowStatements.Exercise;
+
+public class e028_PaintJob {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+        double area = width * height;
+
+
+
+        return 1;
+    }
+
+
+}
