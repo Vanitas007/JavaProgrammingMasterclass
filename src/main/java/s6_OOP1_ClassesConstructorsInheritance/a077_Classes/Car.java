a1791862afd02363e77533e54f7e391c9f74755a
Vanitas007 <piotr.zajac07@gmail.com>
2022-12-18 18:13:39 +0100

    77 Classes, Encapsulation, Setters and Getters in OOP

--- src/main/java/s6_OOP1_ClassesConstructorsInheritance/a077_Classes/Car.java
+++ src/main/java/s6_OOP1_ClassesConstructorsInheritance/a077_Classes/Car.java
@@ -21,11 +21,31 @@ public class Car {
 //        return this.model;
 //    }
 
-    private String make;
-    private String model;
-    private String colour;
-    private int doors;
-    private boolean convertible;
+    private String make = "Tesla";
+    private String model = "Model X";
+    private String colour = "Gray";
+    private int doors = 2;
+    private boolean convertible = true;
+
+    public String getMake(){
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
 
     public void describeCar() {
         System.out.println(doors + "-Door " +
--- src/main/java/s6_OOP1_ClassesConstructorsInheritance/a077_Classes/Main.java
+++ src/main/java/s6_OOP1_ClassesConstructorsInheritance/a077_Classes/Main.java
@@ -2,9 +2,16 @@ package s6_OOP1_ClassesConstructorsInheritance.a077_Classes;
 
 public class Main {
     public static void main(String[] args) {
-        Car porsche = new Car();
-        Car holden = new Car();
-        porsche.setModel("Carrera");
-        System.out.println("Model is "+porsche.getModel());
+//        Car porsche = new Car();
+//        Car holden = new Car();
+//        porsche.setModel("Carrera");
+//        System.out.println("Model is "+porsche.getModel());
+
+
+
+        Car car = new Car();
+        System.out.println("make = "+car.getMake());
+        System.out.println("model = "+ car.getModel());
+        car.describeCar();
     }
 }
