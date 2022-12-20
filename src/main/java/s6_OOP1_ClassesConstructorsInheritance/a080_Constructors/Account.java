299f52333a36a24c0f2cc806312163eaa25bd200
Vanitas007 <piotr.zajac07@gmail.com>
2022-12-20 20:45:13 +0100

    80 Constructors_part1

---
+++ src/main/java/s6_OOP1_ClassesConstructorsInheritance/a080_Constructors/Account.java
@@ -0,0 +1,85 @@
+package s6_OOP1_ClassesConstructorsInheritance.a080_Constructors;
+
+public class Account {
+
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+
+    public Account() {
+        System.out.println("Empty constructor called");
+    }
+
+    public Account(String number, double balance, String customerName,
+                   String email, String phone) {
+        System.out.println("Account constructor with parameters called");
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        customerEmail = email;
+        customerPhone = phone;
+    }
+
+
+    public void depositFunds(double depositAmount) {
+        balance += depositAmount;
+        System.out.println("Deposit of $" + depositAmount
+                + " made. New account balance is $"
+                + balance);
+    }
+
+    public void withdrawMoney(double withrawalAmount) {
+
+        if (balance - withrawalAmount < 0) {
+            System.out.println("You have not enough money! You only have $"
+                    + balance + " in your account.");
+        } else {
+            balance -= withrawalAmount;
+            System.out.println("Withrawal of $" + withrawalAmount
+                    + " processed. Remaining balance = $"
+                    + balance);
+        }
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+}
---
+++ src/main/java/s6_OOP1_ClassesConstructorsInheritance/a080_Constructors/Main.java
@@ -0,0 +1,26 @@
+package s6_OOP1_ClassesConstructorsInheritance.a080_Constructors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Account bobsAccount = new Account("7896658745345", 500,
+                "Mark Twain", "m.twain@gmail.com",
+                "515-234-567");
+
+        System.out.println(bobsAccount.getNumber());
+        System.out.println(bobsAccount.getBalance());
+
+
+//        bobsAccount.setNumber("7896658745345");
+//        bobsAccount.setBalance(500.35);
+//        bobsAccount.setCustomerName("Mark Twain");
+//        bobsAccount.setCustomerEmail("m.twain@gmail.com");
+//        bobsAccount.setCustomerPhone("515-234-567");
+
+        bobsAccount.depositFunds(1500.10);
+        bobsAccount.depositFunds(680.56);
+        bobsAccount.withdrawMoney(2000);
+        bobsAccount.withdrawMoney(1000.67);
+
+    }
+}
