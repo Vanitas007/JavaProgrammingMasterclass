26f97d03e972c888af42fa17ea4be081ae5e8cdd
Vanitas007 <piotr.zajac07@gmail.com>
2022-12-22 23:50:50 +0100

    86 The POJO

---
+++ src/main/java/s6_OOP1_ClassesConstructorsInheritance/a086_POJO/Main.java
@@ -0,0 +1,21 @@
+package s6_OOP1_ClassesConstructorsInheritance.a086_POJO;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            Student s = new Student("S92300" + i,
+                    switch (i) {
+                        case 1 -> "Marry";
+                        case 2 -> "Carol";
+                        case 3 -> "Tim";
+                        case 4 -> "Harry";
+                        case 5 -> "Lisa";
+                        default -> "Anonymous";
+                    },
+                    "05/11/1985",
+                    "Java Masterclass");
+            System.out.println(s);
+        }
+    }
+}
