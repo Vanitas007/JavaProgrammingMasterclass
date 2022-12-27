af680576a7bfa4b4ccbdfaa37e59f7027492cbb2
Vanitas007 <piotr.zajac07@gmail.com>
2022-12-27 22:09:45 +0100

    92 Inheritance_Challenge1

---
+++ src/main/java/s6_OOP1_ClassesConstructorsInheritance/Challenge/a092_Inheritance_Challenge/Main.java
@@ -0,0 +1,16 @@
+package s6_OOP1_ClassesConstructorsInheritance.Challenge.a092_Inheritance_Challenge;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee tim = new Employee("Tim", "11/11/1985", "01/01/2020");
+        System.out.println(tim);
+        System.out.println("Age = " + tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
+
+        Employee joe = new Employee("Joe", "24/12/1991", "14/06/2019");
+        System.out.println(joe);
+        System.out.println("Age = " + joe.getAge());
+        System.out.println("Pay = " + joe.collectPay());
+    }
+}
---
+++ src/main/java/s6_OOP1_ClassesConstructorsInheritance/Challenge/a092_Inheritance_Challenge/SalariedEmployee.java
@@ -0,0 +1,18 @@
+package s6_OOP1_ClassesConstructorsInheritance.Challenge.a092_Inheritance_Challenge;
+
+public class SalariedEmployee {
+    private double annualSalary;
+    private boolean isRetired;
+
+    public SalariedEmployee(double annualSalary, boolean isRetired) {
+        this.annualSalary = annualSalary;
+        this.isRetired = isRetired;
+    }
+
+    public SalariedEmployee() {
+    }
+
+    public void retire() {
+
+    }
+}
