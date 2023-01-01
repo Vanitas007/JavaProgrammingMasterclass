8bf7fd2f3165558e7f35b93f4d060a1097232ef4
Vanitas007 <piotr.zajac07@gmail.com>
2023-01-01 18:17:28 +0100

    CodingExercise_38_Composition

---
+++ src/main/java/s9_OOP2_Polymorphism/Exercise/e038_Composition/Wall.java
@@ -0,0 +1,13 @@
+package s9_OOP2_Polymorphism.Exercise.e038_Composition;
+
+public class Wall {
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
