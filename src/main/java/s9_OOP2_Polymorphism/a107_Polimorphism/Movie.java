e88d125b05771b2b87d6a4f3c2ec6e9f54a5e9cb
Vanitas007 <piotr.zajac07@gmail.com>
2023-01-05 22:20:40 +0100

    108 Polimorphism_2

--- src/main/java/s9_OOP2_Polymorphism/a107_Polimorphism/Movie.java
+++ src/main/java/s9_OOP2_Polymorphism/a107_Polimorphism/Movie.java
@@ -12,6 +12,15 @@ public class Movie {
         String instanceType = this.getClass().getSimpleName();
         System.out.println(title + " is a " + instanceType + " film");
     }
+
+    public static Movie getMovie(String type, String title) {
+        return switch (type.toUpperCase().charAt(0)) {
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new ScienceFiction(title);
+            default -> new Movie(title);
+        };
+    }
 }
 
 class Adventure extends Movie {
@@ -38,6 +47,8 @@ class Comedy extends Movie {
         System.out.printf(".. %s%n".repeat(3), "Something funny happens",
                 "Something even funnier happens", "Happy Ending");
     }
+
+
 }
 
 
