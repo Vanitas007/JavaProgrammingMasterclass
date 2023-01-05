f666ded3935fee0bf526e58602eb2be86f800f57
Vanitas007 <piotr.zajac07@gmail.com>
2023-01-05 21:43:47 +0100

    107 Polimorphism_1

---
+++ src/main/java/s9_OOP2_Polymorphism/a107_Polimorphism/Main.java
@@ -0,0 +1,10 @@
+package s9_OOP2_Polymorphism.a107_Polimorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Movie theMovie = new Adventure("Star Wars");
+        theMovie.watchMovie();
+
+
+    }
+}
