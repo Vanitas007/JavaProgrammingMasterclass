67cee515cfcb3d889a4e632f0911c941a831a84d
Vanitas007 <piotr.zajac07@gmail.com>
2023-01-07 13:24:42 +0100

    111 Polymorphism_Challenge

--- src/main/java/s9_OOP2_Polymorphism/Challenge/a111_Polymorphism_Challenge/Car.java
+++ src/main/java/s9_OOP2_Polymorphism/Challenge/a111_Polymorphism_Challenge/Car.java
@@ -7,38 +7,62 @@ public class Car {
     public Car(String description) {
         this.description = description;
     }
-    public void startEngine(){
 
-    }
-    public void drive(){
-
-    }
-    protected void runEngine(){
-
-    }
-
-    class GasPoweredCar extends Car{
-        public GasPoweredCar(String description) {
-            super(description);
-        }
-        private double accelerate;
-    }
-
-    class ElectricCar extends Car{
-        public ElectricCar(String description) {
-            super(description);
-        }
-        private double accelerate;
-    }
-
-    class HybridCar extends Car{
-        public HybridCar(String description) {
-            super(description);
-        }
-        private double accelerate;
+    public void startEngine() {
+        System.out.println("Car -> startEngine");
     }
 
 
+    protected void runEngine() {
+        System.out.println("Car -> runEngine");
+    }
 
-
+    public void drive() {
+        System.out.println("Car -> driving, type is " + getClass().getSimpleName());
+        runEngine();
+    }
 }
+
+class GasPoweredCar extends Car {
+    private double avgKmPerLiter;
+    private int cylinders = 6;
+
+    public GasPoweredCar(String description) {
+        super(description);
+    }
+
+    public GasPoweredCar(String description, double avgKmPerLiter, int cylinders) {
+        super(description);
+        this.avgKmPerLiter = avgKmPerLiter;
+        this.cylinders = cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Gas -> All %d cylinders are fired up, Ready!%n", cylinders);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Gas -> usage exceeds the average: %.2f %n",avgKmPerLiter);
+    }
+}
+
+class ElectricCar extends Car {
+    public ElectricCar(String description) {
+        super(description);
+    }
+
+    private double accelerate;
+}
+
+class HybridCar extends Car {
+    public HybridCar(String description) {
+        super(description);
+    }
+
+    private double accelerate;
+}
+
+
+
