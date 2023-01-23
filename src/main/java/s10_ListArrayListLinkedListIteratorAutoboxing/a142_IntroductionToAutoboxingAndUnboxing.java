5da6b00a662939b56a1fd09cabd03e606d1d40ff
Vanitas007 <piotr.zajac07@gmail.com>
2023-01-23 21:24:35 +0100

    142 Introduction To Autoboxing And Unboxing

---
+++ src/main/java/s10_ListArrayListLinkedListIteratorAutoboxing/a142_IntroductionToAutoboxingAndUnboxing.java
@@ -0,0 +1,30 @@
+package s10_ListArrayListLinkedListIteratorAutoboxing;
+
+public class a142_IntroductionToAutoboxingAndUnboxing {
+    public static void main(String[] args) {
+
+        // Manual
+        Integer boxedInt = Integer.valueOf(15);                 // preferred, but unnecessary
+        Integer deprecatedBoxing = new Integer(15);          // deprecated since JDK 9
+        int unboxedInt = boxedInt.intValue();                   // unnecessary
+
+        // Automatic
+        Integer autoBoxed = 15;
+        int autoUnboxed = autoBoxed;
+        System.out.println(autoBoxed.getClass().getName());
+//        System.out.println(autoUnboxed.getClass.getName());
+
+        Double resultBoxed = getLiteralDoublePrimitive();
+        double resultUnboxed = getDoubleObject();
+    }
+
+    private static Double getDoubleObject() {
+
+        return Double.valueOf(100.00);
+    }
+
+    private static double getLiteralDoublePrimitive() {
+        return 100.00;
+    }
+
+}
