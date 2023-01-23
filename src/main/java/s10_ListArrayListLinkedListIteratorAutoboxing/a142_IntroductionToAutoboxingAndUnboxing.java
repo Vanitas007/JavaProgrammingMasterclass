fded6ff144622527d1bf966d4a4ec3e3e5467060
Vanitas007 <piotr.zajac07@gmail.com>
2023-01-23 21:57:47 +0100

    143Autoboxing And Unboxing + opcja konfiguracji InteliJ

--- src/main/java/s10_ListArrayListLinkedListIteratorAutoboxing/a142_IntroductionToAutoboxingAndUnboxing.java
+++ src/main/java/s10_ListArrayListLinkedListIteratorAutoboxing/a142_IntroductionToAutoboxingAndUnboxing.java
@@ -1,11 +1,15 @@
 package s10_ListArrayListLinkedListIteratorAutoboxing;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class a142_IntroductionToAutoboxingAndUnboxing {
     public static void main(String[] args) {
 
         // Manual
         Integer boxedInt = Integer.valueOf(15);                 // preferred, but unnecessary
-        Integer deprecatedBoxing = new Integer(15);          // deprecated since JDK 9
+//        Integer deprecatedBoxing = new Integer(15);          // deprecated since JDK 9
         int unboxedInt = boxedInt.intValue();                   // unnecessary
 
         // Automatic
@@ -16,10 +20,39 @@ public class a142_IntroductionToAutoboxingAndUnboxing {
 
         Double resultBoxed = getLiteralDoublePrimitive();
         double resultUnboxed = getDoubleObject();
+
+
+        Integer[] wrapperArray = new Integer[5];
+        wrapperArray[0] = 50;
+        System.out.println(Arrays.toString(wrapperArray));
+
+        System.out.println(wrapperArray[0].getClass().getName());
+
+        Character[] characterArray = {'a', 'b', 'c', 'd'};
+        System.out.println(Arrays.toString(characterArray));
+
+        var ourList = List.of(1,2,3,4,5);
+        System.out.println(ourList);
+
+    }
+
+    private static ArrayList<Integer> getList(Integer... varargs){
+        ArrayList<Integer> aList = new ArrayList<>();
+        for (int i :varargs){
+            aList.add(i);
+        }
+        return aList;
+    }
+
+    private static int returnAnInt(Integer i) {
+        return i;
+    }
+
+    private static Integer returnAnInteger(int i) {
+        return i;
     }
 
     private static Double getDoubleObject() {
-
         return Double.valueOf(100.00);
     }
 
