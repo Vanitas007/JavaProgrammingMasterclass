84b34b97a34bd3223cbf19f115704b0496abcb01
Vanitas007 <piotr.zajac07@gmail.com>
2023-01-24 21:14:52 +0100

    146 Introduction to the enum type

---
+++ src/main/java/s10_ListArrayListLinkedListIteratorAutoboxing/a146_IntroductionToTheEnumType/DayOfTheWeek.java
@@ -0,0 +1,5 @@
+package s10_ListArrayListLinkedListIteratorAutoboxing.a146_IntroductionToTheEnumType;
+
+public enum DayOfTheWeek {
+    SUN, MON, TUES, WED, THURS, FRI, SAT
+}
---
+++ src/main/java/s10_ListArrayListLinkedListIteratorAutoboxing/a146_IntroductionToTheEnumType/Main.java
@@ -0,0 +1,26 @@
+package s10_ListArrayListLinkedListIteratorAutoboxing.a146_IntroductionToTheEnumType;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DayOfTheWeek weekDay = DayOfTheWeek.TUES;
+        System.out.println(weekDay);
+        for (int i = 0; i < 10; i++) {
+            weekDay = getRandomDay();
+            System.out.printf("Name is %s, Ordinal Value = %d%n",
+                    weekDay.name(), weekDay.ordinal());
+
+            if (weekDay == DayOfTheWeek.FRI) {
+                System.out.println("Found a Friday!!!");
+            }
+        }
+    }
+
+    public static DayOfTheWeek getRandomDay() {
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+        return allDays[randomInteger];
+    }
+}
