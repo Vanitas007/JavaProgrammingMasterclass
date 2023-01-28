eec537d043de97ff0da7676dd8313e1d661caacc
Vanitas007 <piotr.zajac07@gmail.com>
2023-01-28 22:43:17 +0100

    CodingExercise_42_Interface

--- src/main/java/s11_InnerAndAbstractClassesAndInterfaces/Exercise/e042_Interface/ISaveable.java
+++ src/main/java/s11_InnerAndAbstractClassesAndInterfaces/Exercise/e042_Interface/ISaveable.java
@@ -1,4 +1,10 @@
 package s11_InnerAndAbstractClassesAndInterfaces.Exercise.e042_Interface;
 
+import java.util.List;
+
 public interface ISaveable {
+
+    List<String> write();
+
+    void read(List<String> savedValues);
 }
---
+++ src/main/java/s11_InnerAndAbstractClassesAndInterfaces/Exercise/e042_Interface/Main.java
@@ -0,0 +1,13 @@
+package s11_InnerAndAbstractClassesAndInterfaces.Exercise.e042_Interface;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player tim = new Player("Tim", 10, 15);
+        System.out.println(tim);
+
+        Monster werewolf = new Monster("Werewolf", 10, 45);
+        System.out.println(werewolf);
+    }
+}
