44693d2a33cd52b0d24314499bb8f62a1904589b
Vanitas007 <piotr.zajac07@gmail.com>
2023-02-04 21:47:07 +0100

    150 Abstract classes_p1

--- src/main/java/s11_New_Abstraction/a150_AbstractClasses/Animal.java
+++ src/main/java/s11_New_Abstraction/a150_AbstractClasses/Animal.java
@@ -1,4 +1,18 @@
 package s11_New_Abstraction.a150_AbstractClasses;
 
-public class Animal {
+public abstract class Animal {
+
+    protected String type;
+    private String size;
+    private double weight;
+
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public abstract void move(String speed);
+
+    public abstract void makeNoise();
 }
---
+++ src/main/java/s11_New_Abstraction/a150_AbstractClasses/Dog.java
@@ -0,0 +1,19 @@
+package s11_New_Abstraction.a150_AbstractClasses;
+
+public class Dog extends Animal {
+
+
+    public Dog(String type, String size, double weight) {
+        super(type, size, weight);
+    }
+
+    @Override
+    public void move(String speed) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+}
