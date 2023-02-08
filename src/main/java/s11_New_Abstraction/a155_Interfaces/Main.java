c09a60b588ac659712b00dbfd27380fdc65f26d1
Vanitas007 <piotr.zajac07@gmail.com>
2023-02-08 23:06:14 +0100

    157 Interfaces_p3

--- src/main/java/s11_New_Abstraction/a155_Interfaces/Main.java
+++ src/main/java/s11_New_Abstraction/a155_Interfaces/Main.java
@@ -1,5 +1,9 @@
 package s11_New_Abstraction.a155_Interfaces;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -27,6 +31,20 @@ public class Main {
         double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
         System.out.printf("The truck traveled %.2f km or %.2f miles%n",
                 kmsTraveled, milesTraveled);
+
+        LinkedList<FlightEnabled> fliers = new ArrayList<>();
+        fliers.add(bird);
+
+        List<FlightEnabled> betterFliers = new LinkedList<>();
+        betterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
     }
 
     private static void inFlight(FlightEnabled flier) {
@@ -36,5 +54,22 @@ public class Main {
             tracked.track();
         }
         flier.land();
+    }
+
+    private static void triggerFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.takeOff();
+        }
+    }
+    private static void flyFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.fly();
+        }
+    }
+
+    private static void landFliers(List<FlightEnabled> fliers) {
+        for (var flier : fliers) {
+            flier.land();
+        }
     }
 }
