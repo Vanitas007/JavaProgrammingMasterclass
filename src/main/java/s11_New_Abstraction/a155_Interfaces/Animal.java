fc75f7b9c43695b0a17d7dcdd7c853e89e52c77a
Vanitas007 <piotr.zajac07@gmail.com>
2023-02-08 21:40:34 +0100

    156 Interfaces_p2

--- src/main/java/s11_New_Abstraction/a155_Interfaces/Animal.java
+++ src/main/java/s11_New_Abstraction/a155_Interfaces/Animal.java
@@ -2,6 +2,9 @@ package s11_New_Abstraction.a155_Interfaces;
 
 interface FlightEnabled {
 
+    double MILES_TO_KM = 1.60934;
+    double KM_TO_MILES = 0.621371;
+
     void takeOff();
 
     void land();
