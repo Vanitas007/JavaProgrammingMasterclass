124a37776de1cea98af344f04e2dd5648614789d
Vanitas007 <piotr.zajac07@gmail.com>
2023-02-09 22:30:04 +0100

    159 Interfaces public static & private methods in JDK8

--- src/main/java/s11_New_Abstraction/a155_Interfaces/Test.java
+++ src/main/java/s11_New_Abstraction/a155_Interfaces/Test.java
@@ -5,7 +5,11 @@ public class Test {
     public static void main(String[] args) {
 
         inFlight(new Jet());
+//        OrbitEarth.log("Testing "+new Satellite());
+
+        orbit(new Satellite());
     }
+
 
     private static void inFlight(FlightEnabled flier) {
         flier.takeOff();
@@ -14,6 +18,12 @@ public class Test {
         if (flier instanceof Trackable tracked) {
             tracked.track();
         }
+        flier.land();
+    }
+
+    private static void orbit(OrbitEarth flier) {
+        flier.takeOff();
+        flier.fly();
         flier.land();
     }
 }
