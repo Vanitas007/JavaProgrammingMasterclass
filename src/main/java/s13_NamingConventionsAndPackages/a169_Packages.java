d7fc885ebe6f6499eac869bbe23ff9ce2b83b592
Vanitas007 <piotr.zajac07@gmail.com>
2023-02-16 23:35:39 +0100

    169 Packages

---
+++ src/main/java/s13_NamingConventionsAndPackages/a169_Packages.java
@@ -0,0 +1,11 @@
+package s13_NamingConventionsAndPackages;
+
+
+public class a169_Packages {
+    public static void main(String[] args) {
+//        Node node = null;
+
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
